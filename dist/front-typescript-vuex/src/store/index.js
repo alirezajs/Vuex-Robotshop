9fd3d2d5bcf3be5a369fa674e0a948960f3b15d7
alirezajs <alireza.varmaghani@gmail.com>
2021-05-10 14:28:39 +0430

    add server

---
+++ dist/front-typescript-vuex/src/store/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const vuex_1 = require("vuex");
+exports.default = vuex_1.createStore({
+    state: {},
+    mutations: {},
+    actions: {},
+    modules: {},
+});
+//# sourceMappingURL=index.js.map
